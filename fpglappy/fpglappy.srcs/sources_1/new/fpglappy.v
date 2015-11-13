1bb64e7e39e6475d36a4bd57d17da40c2c647e33
jmend <jmend@mit.edu>
2015-11-13 13:28:02 -0500

    Created skeleton code (Lab 4). Confirmed synth

---
+++ fpglappy/fpglappy.srcs/sources_1/new/fpglappy.v
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//
+//
+//    ▄████████    ▄██████▄   ▄█          ▄████████    ▄███████▄    ▄███████▄ ▄██   ▄
+//   ███    ███   ███    ███ ███         ███    ███   ███    ███   ███    ███ ███   ██▄
+//   ███    █▀    ███    █▀  ███         ███    ███   ███    ███   ███    ███ ███▄▄▄███
+//  ▄███▄▄▄      ▄███        ███         ███    ███   ███    ███   ███    ███ ▀▀▀▀▀▀███
+// ▀▀███▀▀▀     ▀▀███ ████▄  ███       ▀███████████ ▀█████████▀  ▀█████████▀  ▄██   ███
+//   ███          ███    ███ ███         ███    ███   ███          ███        ███   ███
+//   ███          ███    ███ ███▌    ▄   ███    ███   ███          ███        ███   ███
+//   ███          ████████▀  █████▄▄██   ███    █▀   ▄████▀       ▄████▀       ▀█████▀
+//                           ▀
+//                    ▀█████████▄   ▄█     ▄████████ ████████▄
+//                      ███    ███ ███    ███    ███ ███   ▀███
+//                      ███    ███ ███▌   ███    ███ ███    ███
+//                     ▄███▄▄▄██▀  ███▌  ▄███▄▄▄▄██▀ ███    ███
+//                    ▀▀███▀▀▀██▄  ███▌ ▀▀███▀▀▀▀▀   ███    ███
+//                      ███    ██▄ ███  ▀███████████ ███    ███
+//                      ███    ███ ███    ███    ███ ███   ▄███
+//                    ▄█████████▀  █▀     ███    ███ ████████▀
+//                                        ███    ███
+// Create Date: 11/13/2015 12:51:29 PM
+// Design Name: Fpglappy Bird
+// Module Name: fpglappy
+// Project Name: Fpglappy Bird
+// Target Devices: Nexys 4
+// Tool Versions: Vivado v2015.2
+// Description: Implementation of flappy bird on an FPGA for the 6.111 final
+// project
+//
+// Authors: Julian Mendoza, Wei Low, Nicholas McCoy
+//
+//
+//                   _(_)(_)(_)           _ (_)    _ (_)    _ (_)
+//                 _(_)                  (_)(_)   (_)(_)   (_)(_)
+//                (_) _  _  _               (_)      (_)      (_)
+//                (_)(_)(_)(_)_             (_)      (_)      (_)
+//                (_)        (_)  _  _      (_)      (_)      (_)
+//                (_)_  _  _ (_) (_)(_)   _ (_) _  _ (_) _  _ (_) _
+//                  (_)(_)(_)    (_)(_)  (_)(_)(_)(_)(_)(_)(_)(_)(_)
+//
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+
+module fpglappy(
+    input CLK100MHZ,
+    input[15:0] SW,
+    input BTNC, BTNU, BTNL, BTNR, BTND,
+    output[3:0] VGA_R,
+    output[3:0] VGA_B,
+    output[3:0] VGA_G,
+    output[7:0] JA,
+    output VGA_HS,
+    output VGA_VS,
+    output LED16_B, LED16_G, LED16_R,
+    output LED17_B, LED17_G, LED17_R,
+    output[15:0] LED,
+    output[7:0] SEG,  // segments A-G (0-6), DP (7)
+    output[7:0] AN    // Display 0-7
+    );
+    // Create 25mhz system clock
+    wire clock_25mhz;
+    clock_quarter_divider clockgen(.clk100_mhz(CLK100MHZ), .clock_25mhz(clock_25mhz));
+
+    // Instantiate 7-segment display
+    wire [31:0] data;
+    wire [6:0] segments;
+    display_8hex display(.clk(clock_25mhz),.data(data), .seg(segments), .strobe(AN));    
+    assign SEG[6:0] = segments;
+    assign SEG[7] = 1'b1;
+
+
+    //////////////////////////////////////////////////////////////////////////////////
+    // sample Verilog to generate color bars
+    wire [9:0] hcount;
+    wire [9:0] vcount;
+    wire hsync, vsync, at_display_area;
+    vga vga1(.vga_clock(clock_25mhz),.hcount(hcount),.vcount(vcount),
+          .hsync(hsync),.vsync(vsync),.at_display_area(at_display_area));
+
+    assign VGA_R = at_display_area ? {4{hcount[7]}} : 0;
+    assign VGA_G = at_display_area ? {4{hcount[6]}} : 0;
+    assign VGA_B = at_display_area ? {4{hcount[5]}} : 0;
+    assign VGA_HS = ~hsync;
+    assign VGA_VS = ~vsync;
+
+endmodule
