1fcfed0214bee8c45401f03f804f6ef4485625c8
Nick McCoy <nmccoy@mit.edu>
2015-12-06 03:33:24 -0500

    Added face as sprite

---
+++ fpglappy/fpglappy.srcs/sources_1/ip/blk_mem/blk_mem_stub.vhdl
@@ -0,0 +1,39 @@
+-- Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2015.3 (win64) Build 1368829 Mon Sep 28 20:06:43 MDT 2015
+-- Date        : Sat Dec 05 20:40:52 2015
+-- Host        : DESKTOP-COC4BKA running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/Users/Nick/Documents/fpglappy/fpglappy/fpglappy.srcs/sources_1/ip/blk_mem/blk_mem_stub.vhdl
+-- Design      : blk_mem
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-3
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity blk_mem is
+  Port ( 
+    clka : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 18 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    clkb : in STD_LOGIC;
+    web : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addrb : in STD_LOGIC_VECTOR ( 18 downto 0 );
+    dinb : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end blk_mem;
+
+architecture stub of blk_mem is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,wea[0:0],addra[18:0],dina[7:0],douta[7:0],clkb,web[0:0],addrb[18:0],dinb[7:0],doutb[7:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_3_0,Vivado 2015.3";
+begin
+end;
